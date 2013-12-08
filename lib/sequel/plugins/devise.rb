891bfbccc59d5444738e09fb4f147d7b0c3ae4e4
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2013-12-08 10:47:58 -0200

    Add support for more Devise modules
    fixes #2

--- lib/sequel/plugins/devise.rb
+++ lib/sequel/plugins/devise.rb
@@ -4,6 +4,14 @@ module Sequel
       def self.apply(model, options = {})
         model.extend ::Devise::Models
         model.plugin :hook_class_methods # Devise requires a before_validation
+        model.plugin :dirty # email_changed?
+        model.plugin :validation_class_methods # for using validatable module
+      end
+
+      module InstanceMethods
+        def changed? # For rememberable
+          !changed_columns.empty?
+        end
       end
     end
   end
